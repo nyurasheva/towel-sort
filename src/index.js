7ce772e0ec1ee67c04ebdc2d1c87e7f6e085c554
Анастасия Юрашева <anastasiauraseva@air-anastasia.local>
2022-12-27 02:40:51 +0300

    feat: towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+  return matrix.map((element, index) => {
+    return (index % 2) ? element.reverse() : element;
+  }).flat();
 }
